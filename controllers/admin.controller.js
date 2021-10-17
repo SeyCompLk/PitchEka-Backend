d8fdc85d0978bee6bfc7114be439f03043067bb1
Shakya Peiris <shakyaimanjith32@gmail.com>
2021-10-17 22:07:47 +0530

    intialize controllers

---
+++ controllers/admin.controller.js
@@ -0,0 +1,16 @@
+const User = require('../models/user');
+const Match = require('../models/match');
+const jwt = require('jsonwebtoken');
+const crypto = require('crypto');
+
+exports.postLogin = (req, res, next) => {};
+
+exports.postMatch = (req, res, next) => {};
+
+exports.updateMatch = (req, res, next) => {};
+
+exports.updateScore = (req, res, next) => {};
+
+exports.getMatches = (req, res, next) => {};
+
+exports.getSingleMatch = (req, res, next) => {};
---
+++ controllers/user.controller.js
@@ -0,0 +1,16 @@
+const User = require('../models/user');
+const Match = require('../models/match');
+const jwt = require('jsonwebtoken');
+const crypto = require('crypto');
+
+exports.postRegister = (req, res, next) => {};
+
+exports.postLogin = (req, res, next) => {};
+
+exports.postPrediction = (req, res, next) => {};
+
+exports.postComment = (req, res, next) => {};
+
+exports.getMatches = (req, res, next) => {};
+
+exports.getMatch = (req, res, next) => {};
