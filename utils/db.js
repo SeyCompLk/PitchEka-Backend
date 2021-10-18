e0547ec3a50bde78bb9ae14ed77254c4d0da1782
Shakya Peiris <shakyaimanjith32@gmail.com>
2021-10-18 17:46:27 +0530

    change multiple args into an object

--- utils/db.js
+++ utils/db.js
@@ -1,12 +1,22 @@
 const mongoose = require('mongoose');
 const { MongoMemoryServer } = require('mongodb-memory-server');
+const bcrypt = require('bcrypt');
 
-exports.connectMock = () => {
+exports.connectMock = (args) => {
+    let admins = args.admins;
     return new Promise((resolve, reject) => {
         MongoMemoryServer.create()
             .then((mongoServer) => {
                 mongoose.connect(mongoServer.getUri()).then((client) => {
-                    resolve();
+                    const Admin = client.model('Admin');
+                    admins.forEach((admin) => {
+                        bcrypt.hash(admin.password, 10, (err, hash) => {
+                            admin.password = hash;
+                        });
+                    });
+                    Admin.insertMany(admins).then(() => {
+                        resolve();
+                    });
                 });
             })
             .catch((err) => reject(err));
