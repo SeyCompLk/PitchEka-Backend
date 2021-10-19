33888a3ccb21b7bb066998ba1fddb915c12b0499
Shakya Peiris <shakyaimanjith32@gmail.com>
2021-10-19 15:51:18 +0530

    set to conditionally import mongo memory server

--- utils/db.js
+++ utils/db.js
@@ -1,39 +1,41 @@
 const mongoose = require('mongoose');
-const { MongoMemoryServer } = require('mongodb-memory-server');
 const bcrypt = require('bcrypt');
 
 exports.connectMock = (args) => {
     let admins = args.admins;
     let users = args.users;
-    return new Promise((resolve, reject) => {
-        MongoMemoryServer.create()
-            .then((mongoServer) => {
-                mongoose.connect(mongoServer.getUri()).then((client) => {
-                    if (admins) {
-                        const Admin = client.model('Admin');
-                        admins.forEach((admin) => {
-                            bcrypt.hash(admin.password, 10, (err, hash) => {
-                                admin.password = hash;
+    if (process.env.NODE_ENV == 'test') {
+        const { MongoMemoryServer } = require('mongodb-memory-server');
+        return new Promise((resolve, reject) => {
+            MongoMemoryServer.create()
+                .then((mongoServer) => {
+                    mongoose.connect(mongoServer.getUri()).then((client) => {
+                        if (admins) {
+                            const Admin = client.model('Admin');
+                            admins.forEach((admin) => {
+                                bcrypt.hash(admin.password, 10, (err, hash) => {
+                                    admin.password = hash;
+                                });
                             });
-                        });
-                        Admin.insertMany(admins).then(() => {
-                            resolve();
-                        });
-                    } else if (users) {
-                        const User = client.model('User');
-                        users.forEach((user) => {
-                            bcrypt.hash(user.password, 10, (err, hash) => {
-                                user.password = hash;
+                            Admin.insertMany(admins).then(() => {
+                                resolve();
                             });
-                        });
-                        User.insertMany(users).then(() => {
-                            resolve();
-                        });
-                    }
-                });
-            })
-            .catch((err) => reject(err));
-    });
+                        } else if (users) {
+                            const User = client.model('User');
+                            users.forEach((user) => {
+                                bcrypt.hash(user.password, 10, (err, hash) => {
+                                    user.password = hash;
+                                });
+                            });
+                            User.insertMany(users).then(() => {
+                                resolve();
+                            });
+                        }
+                    });
+                })
+                .catch((err) => reject(err));
+        });
+    }
 };
 
 exports.connect = () => {
