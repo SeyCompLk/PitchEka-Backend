f484076f6e27e8fe61e885655a406156d36c8e50
Shakya Peiris <shakyaimanjith32@gmail.com>
2021-10-19 15:25:12 +0530

    fix: change the placement of initial route

--- app.js
+++ app.js
@@ -12,14 +12,14 @@ require('dotenv').config();
 const db = require('./utils/db');
 if (process.env.NODE_ENV != 'test') db.connect();
 
-app.use('/', (req, res, next) => {
-    res.send('Welcome to the pitch eka API');
-});
-
 const adminRoutes = require('./routes/admin.routes');
 app.use('/admin', adminRoutes);
 
 const userRoutes = require('./routes/user.routes');
 app.use('/user', userRoutes);
+
+app.use('/', (req, res, next) => {
+    res.send('Welcome to the pitch eka API');
+});
 
 module.exports = app;
