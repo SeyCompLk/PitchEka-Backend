6e1ac9ea24edc33ae559d697f7d6ce5a0276e76f
Shakya Peiris <shakyaimanjith32@gmail.com>
2021-10-21 21:08:55 +0530

    update verify user token logic to suit nob-token requests for matches

--- utils/verifyToken.js
+++ utils/verifyToken.js
@@ -3,7 +3,11 @@ const jwt = require('jsonwebtoken');
 exports.verifyUser = (req, res, next) => {
     const token =
         req.body.token || req.query.token || req.headers['x-access-token'];
-
+    // console.log(req);
+    if (req.url.includes('/matches') && !token) {
+        req.verified = false;
+        return next();
+    }
     if (!token) {
         return res.status(403).send({
             success: false,
@@ -17,6 +21,8 @@ exports.verifyUser = (req, res, next) => {
                 suceess: false,
                 message: 'Need user account',
             });
+        req.user = decoded.email;
+        req.verified = true;
     } catch (err) {
         return res
             .status(401)
@@ -37,11 +43,12 @@ exports.verifyAdmin = (req, res, next) => {
     }
     try {
         const decoded = jwt.verify(token, process.env.TOKEN_KEY);
-        if (!decoded.isAdmin)
+        if (!decoded.isAdmin || !decoded.verified)
             return res.status(400).send({
                 suceess: false,
                 message: 'Need admin access',
             });
+        req.user = decoded.email;
     } catch (err) {
         return res
             .status(401)
